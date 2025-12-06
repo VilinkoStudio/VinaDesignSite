467e2a120791a3c465ebe6e3fa1472dd7db5e0be
yuyudifiesh <yizhixianyuuuu@gmail.com>
2025-12-07 02:29:17 +0800

    chore: 初始化项目并添加自动部署配置

---
+++ .vitepress/config.mjs
@@ -0,0 +1,81 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Vina Design",
+  description: "Vina 设计规范",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '设计', link: '/docs/design/values' },
+      { text: '实践', link: '/docs/practice/' },
+      { text: '关于', link: '/docs/about/' },
+    ],
+
+    search: {
+      provider: "local",
+      options: {
+        enablePhraseSearch: true,
+        translations: {
+          button: {
+            buttonText: "搜索",
+            buttonAriaLabel: "搜索"
+          },
+          modal: {
+            noResultsText: "未找到结果",
+            resetButtonTitle: "重置",
+            footer: {
+              selectLabel: "选择",
+              navigateUpKey: "↑",
+              navigateDownKey: "↓",
+              closeKey: "Esc",
+              submitKey: "Enter"
+            },
+          },
+        },
+      },
+    },
+
+    sidebar: [
+      {
+        text: 'Vina Design',
+        items: [
+          { text: '价值观', link: '/docs/design/values' }
+        ]
+      },
+      {
+        text: '设计',
+        items: [
+          { text: 'Color 色彩', link: '/docs/design/color' },
+          { text: 'Controls 控件', link: '/docs/design/controls' },
+          { text: 'Layout 布局', link: '/docs/design/layout' },
+          { text: 'Motion 动效', link: '/docs/design/motion' },
+          { text: 'Dark Mode 深色模式', link: '/docs/design/dark-mode' },
+        ]
+      },
+      {
+        text: '实践',
+        items: [
+        ]
+      },
+    ],
+
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+    sidebarMenuLabel: '菜单',
+    outline: { level: [2, 3], label: '目录' },
+    returnToTopLabel: '返回顶部',
+    docFooter: { prev: '上一篇', next: '下一篇' },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/VilinkoStudio' }
+    ],
+
+    footer: {
+      copyright: 'Copyright © 2019 - 2025 Vilinko Studio. All Rights Reserved.',
+      message: '<a href="https://beian.miit.gov.cn/" target="_blank" style="text-decoration: none;">粤ICP备2025454076号</a>'
+    },
+  }
+})
---
+++ index.md
@@ -0,0 +1,20 @@
+---
+layout: home
+
+hero:
+  name: "Vina Design"
+  text: "We Meet, Inspire, Create"
+
+features:
+  - icon: 🪄
+    title: 设计
+    details: 了解我们的设计价值观和原则。
+    link: /docs/design/values
+    linkText: 了解更多
+  - icon: 🚀
+    title: 实践
+    details: 查看设计规范的最佳实践。
+    link: /docs/practice/index
+    linkText: 了解更多
+---
+
